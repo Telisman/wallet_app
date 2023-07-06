30491c7b942dd3280e475f13182a020827b441b5
Davor Telisman <davortelisman@gmail.com>
2023-07-06 09:47:26 +0200

    Create Transaction model in are microservices wallet_api

--- wallet_api/models.py
+++ wallet_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from wallet_processor.models import Customer
 
-# Create your models here.
+class Transaction(models.Model):
+    value = models.FloatField()
+    latency = models.IntegerField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.value}: {self.latency} {self.customer}"
